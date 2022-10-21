2c2eb0282fc5a12cb0e64ea6f16de30bd2a44b7a
juraantoniv <juraantoniv@gmail.com>
2022-10-21 14:37:04 +0300

    ok

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,120 +1,3 @@
-// import React, {useEffect, useState} from 'react';
-// import {useNavigate, useSearchParams} from "react-router-dom";
-// import css from "./Header.module.css"
-// import {useDispatch, useSelector} from "react-redux";
-// import {getMovies} from "../../redax";
-// import {Navbar} from "react-bootstrap";
-// import {Switch} from "./Switch";
-// import async from "async";
-//
-// const Header = () => {
-//
-//
-//     const [query, setQuery] = useState("")
-//     const [prev, setPrev] = useState(1);
-//     const [next, setNext] = useState(1);
-//     // const [value, setValue] = useState(false)
-//     // const [darkTheme, setDarkTheme] = useState(false)
-//
-//     let [queryPage, setQueryPage,page] = useSearchParams({page:'1'});
-//
-//
-//
-//
-//     const dispatch = useDispatch()
-//     const {name,configuration}=useSelector(state => state.movieReducer)
-//
-//     useEffect(() => {
-//         dispatch(getMovies.getName())
-//     },[])
-//
-//
-//     useEffect(() => {
-//         dispatch(getMovies.getConfiguration())
-//     },[])
-//
-//
-//     page = queryPage.get('page');
-//
-//
-//     useEffect(()=>{
-//        dispatch(getMovies.getAllMovies(queryPage.get('page')))
-//
-//    },[dispatch, next, prev,queryPage])
-//
-//
-//
-//     useEffect(() => {
-//         dispatch(getMovies.getMovie(query))
-//     },[dispatch, query])
-//
-//
-//
-//
-//     const prevPage = () => {
-//         setQueryPage(value=>({page:value.get('page')-1}))
-//         setPrev(prev-1)
-//         setNext(next-1)
-//
-//
-//     }
-//     const nextPage = () => {
-//         setQueryPage(value=>({page:+value.get('page')+1}))
-//         setNext(next+1)
-//         setPrev(prev+1)
-//
-//     }
-//
-//
-//
-//     const navigate = useNavigate();
-//
-//     return (
-//         <div>
-//             <div className={css.Header}>
-//                 <div>
-//                     <div>
-//                        {/*<img src={'src/components/Header/Img/Знімок екрана 2022-10-19 143920.jpg'} alt={'img'}/>*/}
-//                     </div>
-//                     <div></div>
-//                     <div></div>
-//                     <div></div>
-//                 </div>
-//                 <div>
-//
-//             <div>
-//                 <form className={css.search}>
-//                     <input type="text"
-//                            placeholder={"Search Movie"}
-//                            className={css.searchTerm}
-//                            onChange={event => setQuery(event.target.value)}
-//                            value={query}
-//                     />
-//                 </form>
-//                 <button onClick={() => navigate('53')} className={css.btn}>Action</button>
-//                 <button onClick={() => navigate('12')} className={css.btn}>Adventure</button>
-//                 <button onClick={() => navigate('35')} className={css.btn}>Comedy</button>
-//                 <button onClick={() => navigate('80')} className={css.btn}>Crime</button>
-//                 <button onClick={() => navigate('99')} className={css.btn}>Documentary</button>
-//
-//             </div>
-//
-//             </div>
-//             <div className={css.name}>
-//                 <h3>Welcome : {name.username}</h3>
-//             </div>
-//
-//             </div>
-//             <button className={css.btn1} disabled={+page===1} onClick={prevPage}>prevPage</button>
-//             <button className={css.btn1} disabled={+page===500} onClick={nextPage}>nextPage</button>
-//         </div>
-//     );
-// };
-//
-// export {
-//     Header
-// }
-//
 
 import React, {useEffect, useState} from 'react';
 import {useNavigate, useParams, useSearchParams} from "react-router-dom";
@@ -123,6 +6,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {getMovies} from "../../redax";
 import {Switch} from "./Switch";
 import useLocalStorage from "use-local-storage";
+import css1 from "./Header.module.css";
 
 const Header = () => {
 
@@ -254,7 +138,7 @@ const Header = () => {
                         <div>
                         {/*<Switch/>*/}
                         </div>
-                        {/*<img src={'src/components/Header/Img/Знімок екрана 2022-10-19 143920.jpg'} alt={'img'}/>*/}
+                        <button onClick={() => navigate('/Movies')} className={css.btn_top_page}>Home Page</button>
                     </div>
                     <div></div>
                     <div></div>
@@ -281,7 +165,7 @@ const Header = () => {
                 </div>
                 <div className={css.name}>
                     <h3>Welcome : {name.username}</h3>
-                    <img className={css.image_name} src={`http://image.tmdb.org/t/p/w500/${name.avatar.tmdb.avatar_path}`} alt='img_at'/>
+                    <img className={css.image_name} src={`http://image.tmdb.org/t/p/w500/${name.avatar?.tmdb.avatar_path}`} alt='img_at'/>
 
                 </div>
 
